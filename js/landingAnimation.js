189ed5eb0322c746436e356f2767a538d1a3ec3b
mihir shah <mihir.shah@tatvamail.com>
2023-12-18 15:57:10 +0530

    initial-commit

---
+++ js/landingAnimation.js
@@ -0,0 +1,89 @@
+$(".car-slider").slick({
+    dots: true,
+    // infinite: true,
+    // speed: 300,
+    // slidesToShow: 4,
+    // slideToScroll: 2,
+    // autoplaySpeed: 2000,
+    // autoplay: true,
+    // centerMode: true,
+    variableWidth: true,
+    prevArrow: false,
+    nextArrow: false,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slideToScroll: 2,
+            },
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 3,
+                slideToScroll: 1,
+
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ],
+});
+
+
+// Testimonials 
+$(".slider-thumb").slick({
+    // dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 7,
+    slideToScroll: 1,
+    autoplaySpeed: 2000,
+    // autoplay: true,
+    centerMode: true,
+    asNavFor: '.slider-content',
+    variableWidth: true,
+    prevArrow: "<button type='button' class='slick-prev btn'><img src='./images/ic-arrow-prev.svg' alt='Prev Icon'></button>",
+    nextArrow: "<button type='button' class='slick-next btn'><img src='./images/ic-arrow-next.svg' alt='Next Icon'></button>",
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slideToScroll: 1,
+            },
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 3,
+                slideToScroll: 1,
+
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ],
+});
+$('.slider-content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: true,
+    // speed: 1000,
+    adaptiveHeight: true,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-thumb',
+});
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// JS Which needs to add
+document.write(	
+	'<script src="js/jquery.js"></script>'+	
+	'<script src="js/slick.min.js"></script>' +
+	'<script src="js/bootstrap.bundle.min.js"></script>'+
+	'<script src="js/custom.js"></script>'
+	);
